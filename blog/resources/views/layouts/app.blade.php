1ca7857c089161d14d82e03f1a2d3e674de09890
maggie97 <maggielee97@icloud.com>
2020-04-23 16:48:45 -0500

    Add a field for a scripts in the another views

--- blog/resources/views/layouts/app.blade.php
+++ blog/resources/views/layouts/app.blade.php
@@ -13,8 +13,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <script src="{{asset('js/jquery.min.js')}}"></script>
+    @yield('scripts')
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
