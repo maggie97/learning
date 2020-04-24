68366b157c22cf3db7ba334f44cf988e280b9d85
maggie97 <maggielee97@icloud.com>
2020-04-24 17:35:51 -0500

    rename the name of the route in game.blade

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -12,12 +12,12 @@
     <div class="row justify-content-center">
         <div class="col-4" style="height: 200px">
             <a href="{{ route('bubbles') }}">
-                <img src="{{ url('/img/bubblesGame.png')}}" alt="" style="width:100%; height: 100%">
+                <img src="{{ url('img/bubblesGame.png')}}" alt="" style="width:100%; height: 100%">
             </a>
         </div>
         <div class="col-4" style="height: 200px; width:100% ">
             <a href="{{ route('memory')}}">
-                <img src="{{ url('/img/memoryGame.jpg')}}" alt="" style="width:100%; height: 100%">
+                <img src="{{ url('img/memoryGame.jpg')}}" alt="" style="width:100%; height: 100%">
             </a>
         </div>
     </div>
