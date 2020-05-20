67a0787e9f8cf997cb4908de42e4c55977b6475e
maggie97 <maggielee97@icloud.com>
2020-05-20 14:48:01 -0500

    edit profile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Perfil</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +13,41 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    <form action="">
+                        <div class="form-group row">
+                            <label for="exampleInputEmail1"  class="col-sm-2 col-form-label">Nombre:</label>
+                            <div class="col-sm-10">
+                                <input type="text"  class="form-control-plaintext" id="exampleInputNAme" aria-describedby="emailHelp" value="{{Auth::user()->name}}">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="exampleInputEmail1"  class="col-sm-2 col-form-label">Apellidos:</label>
+                            <div class="col-sm-10">
+                                <input type="text"  class="form-control-plaintext" id="exampleInputLastname" value="{{Auth::user()->lastName}}">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="exampleInputEmail1"  class="col-sm-2 col-form-label">Nombre:</label>
+                            <div class="col-sm-8">
+                                <input type="email"  class="form-control-plaintext" id="exampleInputName" value="{{Auth::user()->name}}">
+                            </div>
+                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                            <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                        </div>
+                        <div class="form-group row">
+                            <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                              <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="{{Auth::user()->email}}">
+                              
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="exampleInputEmail1"  class="col-sm-2 col-form-label">Contraseña:</label>
+                            <div class="col-sm-10">
+                                <input type="password"  class="form-control-plaintext" id="exampleInputPassword"  value="**********">
+                            </div>
+                        </div>
+                    </form>
                 </div>
                 
             </div>
