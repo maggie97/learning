27739c676c5559bccaa73ae9f1a6d8fce733ff96
maggie97 <maggielee97@icloud.com>
2020-05-27 20:57:32 -0500

    add names in the routes

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,15 @@ Route::get('/game/bubbles', 'GameController@bubbles')->name('bubbles');
 Route::get('/game/memory', 'GameController@memory')->name('memory');
 Route::get('/game', 'GameController@index')->name('games');
 
+Route::resource('/children', 'ChildController', ['names' => [
+        'index' => 'children',
+        'create' => 'child.new', 
+        'store' => 'child.store',
+        'edit' => 'child.edit',
+        'update' => 'child.update',
+        'destroy' => 'child.delete'
+    ]
+]);
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('/children', 'ChildController');
